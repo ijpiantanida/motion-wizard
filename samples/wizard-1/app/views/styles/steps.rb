e8086b3bb2836620f8a7e69bfeef5a863ffd0941
Ignacio Piantanida <ijpiantanida@gmail.com>
2013-09-21 14:51:46 -0300

    Made wizard-1 texts and buttons taller

--- samples/wizard-1/app/views/styles/steps.rb
+++ samples/wizard-1/app/views/styles/steps.rb
@@ -3,7 +3,7 @@ Teacup::Stylesheet.new :steps do
         backgroundColor: :white.uicolor
 
   style :big_button,
-        size: ["100% - 40", 30],
+        size: ["100% - 40", 35],
         center_x: "50%",
         backgroundColor: [103,189,71].uicolor
 
@@ -14,7 +14,7 @@ Teacup::Stylesheet.new :step_1 do
 
   style :text_field,
     placeholder: "Put some text here",
-    size: ["100% - 40", 30],
+    size: ["100% - 40", 35],
     top: 130,
     center_x: "50%",
     textAlignment: NSTextAlignmentCenter,
@@ -35,7 +35,7 @@ Teacup::Stylesheet.new :step_2 do
 
   style :text_field,
         placeholder: "Also add some text here",
-        size: ["100% - 40", 30],
+        size: ["100% - 40", 35],
         top: 130,
         center_x: "50%",
         textAlignment: NSTextAlignmentCenter,
@@ -46,7 +46,7 @@ Teacup::Stylesheet.new :step_2 do
         }
 
   style :base_button,
-        size: ["50% - 40", 30],
+        size: ["50% - 40", 35],
         backgroundColor: [103,189,71].uicolor
 
   style :next_button,
@@ -65,14 +65,14 @@ Teacup::Stylesheet.new :step_3 do
 
   style :label,
         text: "Not much to do around here",
-        size: ["100% - 40", 30],
+        size: ["100% - 40", 35],
         top: 130,
         center_x: "50%",
         textAlignment: NSTextAlignmentCenter,
         textColor: [103,189,71].uicolor
 
   style :base_button,
-        size: ["50% - 40", 30],
+        size: ["50% - 40", 35],
         backgroundColor: [103,189,71].uicolor
 
   style :next_button,
@@ -91,14 +91,14 @@ Teacup::Stylesheet.new :step_4 do
 
   style :label,
         text: "End of the journey",
-        size: ["100% - 40", 30],
+        size: ["100% - 40", 35],
         top: 130,
         center_x: "50%",
         textAlignment: NSTextAlignmentCenter,
         textColor: [103,189,71].uicolor
 
   style :data,
-        size: ["100% - 40", 30],
+        size: ["100% - 40", 35],
         center_x: "50%",
         textAlignment: NSTextAlignmentCenter,
         textColor: [103,189,71].uicolor
